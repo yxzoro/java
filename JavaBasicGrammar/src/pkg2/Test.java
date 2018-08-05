2628874dddb0eb02b87ab38e4e19c00871c204af
yxzoro <yangxing20107@gmail.com>
2018-08-05 15:35:32 +0800

    .

---
+++ JavaBasicGrammar/src/pkg2/Test.java
@@ -0,0 +1,20 @@
+package pkg2;
+
+import java.util.HashMap;
+import java.util.LinkedList;
+
+public class Test{
+    public void xyz(){
+        System.out.print("");
+    }
+    
+    public void zyx() {
+    	HashMap map = new HashMap();
+        map.get("");
+        
+        LinkedList list = new LinkedList();
+        list.add(0, "element");
+    }
+
+}
+
