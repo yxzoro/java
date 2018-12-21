1c2a7ca17d85d8976625d96af05478f433415130
yangxing <yangxing@gosun.com>
2018-12-21 06:08:33 -0800

    .

--- gradle-java-projects-examples/socket_server_demo/src/main/java/javaapp/App.java
+++ gradle-java-projects-examples/socket_server_demo/src/main/java/javaapp/App.java
@@ -1,11 +1,16 @@
 package javaapp;
 
-import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.Scanner;
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.nio.Buffer;
+
+import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ExecutorService;
@@ -14,7 +19,7 @@ import java.util.concurrent.ExecutorService;
 public class App{
     public static void main(String[] args) throws IOException {
         ServerSocket server_socket = new ServerSocket(18000);
-        ExecutorService executor = Executors.newFixedThreadPool(20);
+        ExecutorService executor = Executors.newFixedThreadPool(10);
         try {
             System.out.println("Server socket started at :18000.");
             // use threadpool to handle connections            
@@ -44,14 +49,35 @@ class WorkerThread implements Runnable {
     @Override
     public void run() {        
         try {
-            // read data from InputStream to byte[]
+            // read data from InputStream to byte[], then convert byte[] to string.
             byte[] buffer = new byte[1024];
             this.sock.getInputStream().read(buffer);
-            System.out.println("=> got data from" + this.sock.toString() + "\n" + buffer);
+            System.out.println("=> got data bytes from" + this.sock.toString());
+            System.out.println(Arrays.toString(buffer));
+            System.out.println("=> got data string from" + this.sock.toString());
+            System.out.println(new String(buffer));
+            /*  
+             in java: 
+             byte[] to string:   String s = new String(new byte[]{110, 112, 113});
+             string to byte[]:   byte[] bytes = "asd".getBytes(); 
+             print byte[]:       Arrays.toString(bytes); 
+            */
+ 
+            // read string from socket by Scanner
+            Scanner in = new Scanner(this.sock.getInputStream());
+            if (in.hasNext()) {
+                System.out.println("=> got second data string from" + this.sock.toString());
+                System.out.println(in.next());
+            }
+         
+            // write string to socket by PrintWriter
             PrintWriter out = new PrintWriter(this.sock.getOutputStream(), true);
             out.println(new Date().toString());
+
+            // close socket
             this.sock.close();
         } catch (IOException e) {
+            e.printStackTrace();
         }
         finally {            
         }
