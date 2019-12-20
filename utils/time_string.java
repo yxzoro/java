b78ebdd8b7da5b819c0064069ebf13d6a19c3edb
Michael Yang <yangxing20107@gmail.com>
2019-12-20 11:09:41 +0800

    Create time_string.java

---
+++ utils/time_string.java
@@ -0,0 +1,11 @@
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+SimpleDateFormat F = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+String dateString = F.format(new Date());
+    
+    
+    
+    
+    
+    
