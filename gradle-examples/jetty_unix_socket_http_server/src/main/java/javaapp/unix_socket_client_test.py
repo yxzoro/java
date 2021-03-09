fc4b3b9c62f622c396f45f7c4f81d64341c21cef
Michael Yang <yangxing20107@gmail.com>
2021-03-09 21:17:11 +0800

    Update unix_socket_client_test.py

--- gradle-examples/jetty_unix_socket_http_server/src/main/java/javaapp/unix_socket_client_test.py
+++ gradle-examples/jetty_unix_socket_http_server/src/main/java/javaapp/unix_socket_client_test.py
@@ -10,7 +10,11 @@ print(time.time() - t1)
 print(r.status_code)
 print(r.content)
 
-#TODO: 进程通信，使用unix socket方式和ip:port方式一次请求都是10ms左右，性能并没什么区别 ？？
+# TODO: 
+# 进程通信，使用unix socket方式和ip:port方式一次请求都是10ms左右，性能并没什么区别 ??
+# 理论上应该是unix socket方式会更快(不走网络协议栈,绕过内核态拷贝过程),但是测试的结果都是10ms左右,难道是我测试不标准 ?? 
+# 不过至少说明两种方式都能采用,耗时不过10ms而已嘛.
+# 以后这就是java对接python的方式了!
 
 # 2.http client, connect to http://localhost:port
 # Time: 10ms
